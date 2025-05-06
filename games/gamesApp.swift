b715e6f3709c6ac8da995522ef2e71a06288d116
Bhashit Gautam <bhashitgtm004@gmail.com>
2025-05-06 18:21:33 +0530

    Initial Commit

---
+++ games/gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamesApp.swift
+//  games
+//
+//  Created by Bhashit Gautam on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
